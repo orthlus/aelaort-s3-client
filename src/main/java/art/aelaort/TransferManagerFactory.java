15750f99d4eadc05c59bd108451a85b0349bbb42
ort <orthodoxplus@ya.ru>
2024-04-25 20:35:22 +0300

    tm

--- src/main/java/art/aelaort/TransferManagerFactory.java
+++ src/main/java/art/aelaort/TransferManagerFactory.java
@@ -3,7 +3,7 @@ package art.aelaort;
 import com.amazonaws.services.s3.AmazonS3;
 
 public class TransferManagerFactory {
-	public static TransferManagerResource of(S3Parameters s3Parameters) {
+	public static TransferManagerResource tm(S3Parameters s3Parameters) {
 		AmazonS3 client = S3ClientFactory.client(s3Parameters);
 
 		return new TransferManagerResource(client);
