113c07d6d6cc95169ec2762a2257108f9277ad57
ort <orthodoxplus@ya.ru>
2024-04-28 00:12:30 +0300

    clean

--- src/main/java/art/aelaort/S3ClientResource.java
+++ src/main/java/art/aelaort/S3ClientResource.java
@@ -1,13 +1,9 @@
 package art.aelaort;
 
 import com.amazonaws.services.s3.AmazonS3;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.Accessors;
 import lombok.experimental.Delegate;
 
-@Getter
-@Accessors(fluent = true)
 @RequiredArgsConstructor
 public class S3ClientResource implements AutoCloseable {
 	@Delegate
