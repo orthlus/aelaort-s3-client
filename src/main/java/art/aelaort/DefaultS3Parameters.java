c6133424d90851c72f185a6b763321be7bb52698
ort <orthodoxplus@ya.ru>
2024-04-28 00:07:08 +0300

    lombok

--- src/main/java/art/aelaort/DefaultS3Parameters.java
+++ src/main/java/art/aelaort/DefaultS3Parameters.java
@@ -1,35 +1,13 @@
 package art.aelaort;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public class DefaultS3Parameters implements S3Parameters {
 	private final String id;
 	private final String key;
 	private final String url;
 	private final String region;
-
-	public DefaultS3Parameters(String id, String key, String url, String region) {
-		this.id = id;
-		this.key = key;
-		this.url = url;
-		this.region = region;
-	}
-
-	@Override
-	public String getId() {
-		return id;
-	}
-
-	@Override
-	public String getKey() {
-		return key;
-	}
-
-	@Override
-	public String getUrl() {
-		return url;
-	}
-
-	@Override
-	public String getRegion() {
-		return region;
-	}
 }
