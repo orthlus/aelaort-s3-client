adc7e2ed83139d92bfe2f868cc6830646d88e54f
ort <orthodoxplus@ya.ru>
2024-04-29 01:03:01 +0300

    tm threads count

--- src/main/java/art/aelaort/TransferManagerFactory.java
+++ src/main/java/art/aelaort/TransferManagerFactory.java
@@ -8,4 +8,10 @@ public class TransferManagerFactory {
 
 		return new TransferManagerResource(client);
 	}
+
+	public static TransferManagerResource tm(S3Parameters s3Parameters, int nThreads) {
+		AmazonS3 client = S3ClientFactory.client(s3Parameters);
+
+		return new TransferManagerResource(client, nThreads);
+	}
 }
--- src/main/java/art/aelaort/TransferManagerResource.java
+++ src/main/java/art/aelaort/TransferManagerResource.java
@@ -4,9 +4,17 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 
+import java.util.concurrent.Executors;
+
 public class TransferManagerResource extends TransferManager implements AutoCloseable {
 	TransferManagerResource(AmazonS3 client) {
 		super(TransferManagerBuilder.standard().withS3Client(client));
+	}
+
+	TransferManagerResource(AmazonS3 client, int nThreads) {
+		super(TransferManagerBuilder.standard()
+				.withExecutorFactory(() -> Executors.newFixedThreadPool(nThreads))
+				.withS3Client(client));
 	}
 
 	@Override
