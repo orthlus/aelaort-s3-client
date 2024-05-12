60da6cfd97ccfe36d084fa9f773a3faa2a18c8aa
ort <orthodoxplus@ya.ru>
2024-05-13 01:29:51 +0300

    minio

--- src/main/java/art/aelaort/S3ClientFactory.java
+++ src/main/java/art/aelaort/S3ClientFactory.java
@@ -1,5 +1,6 @@
 package art.aelaort;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -7,8 +8,20 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 public class S3ClientFactory {
+	public static S3ClientResource forMinio(S3Parameters s3Parameters) {
+		return new S3ClientResource(clientForMinio(s3Parameters));
+	}
+
 	public static S3ClientResource of(S3Parameters s3Parameters) {
 		return new S3ClientResource(client(s3Parameters));
+	}
+
+	static AmazonS3 clientForMinio(S3Parameters s3Parameters) {
+		return clientForMinio(
+				s3Parameters.getId(),
+				s3Parameters.getKey(),
+				s3Parameters.getUrl(),
+				s3Parameters.getRegion());
 	}
 
 	static AmazonS3 client(S3Parameters s3Parameters) {
@@ -17,6 +30,21 @@ public class S3ClientFactory {
 				s3Parameters.getKey(),
 				s3Parameters.getUrl(),
 				s3Parameters.getRegion());
+	}
+
+	static AmazonS3 clientForMinio(String id, String key, String url, String region) {
+		ClientConfiguration clientConfiguration = new ClientConfiguration()
+				.withSignerOverride("AWSS3V4SignerType");
+		BasicAWSCredentials credentials = new BasicAWSCredentials(id, key);
+		AWSStaticCredentialsProvider provider = new AWSStaticCredentialsProvider(credentials);
+		AwsClientBuilder.EndpointConfiguration configuration =
+				new AmazonS3ClientBuilder.EndpointConfiguration(url, region);
+		return AmazonS3ClientBuilder.standard()
+				.withClientConfiguration(clientConfiguration)
+				.withCredentials(provider)
+				.withEndpointConfiguration(configuration)
+				.enablePathStyleAccess()
+				.build();
 	}
 
 	static AmazonS3 client(String id, String key, String url, String region) {
--- src/main/java/art/aelaort/TransferManagerFactory.java
+++ src/main/java/art/aelaort/TransferManagerFactory.java
@@ -3,6 +3,12 @@ package art.aelaort;
 import com.amazonaws.services.s3.AmazonS3;
 
 public class TransferManagerFactory {
+	public static TransferManagerResource minioTm(S3Parameters s3Parameters) {
+		AmazonS3 client = S3ClientFactory.clientForMinio(s3Parameters);
+
+		return new TransferManagerResource(client);
+	}
+
 	public static TransferManagerResource tm(S3Parameters s3Parameters) {
 		AmazonS3 client = S3ClientFactory.client(s3Parameters);
 
